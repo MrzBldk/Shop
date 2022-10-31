24ecdc383d74696880ce71f63c59db93e9368d0d
MrzBldk <anton_lityagin@mail.ru>
2022-10-31 18:29:50 +0300

    tests for apllication layer logic

---
+++ Services/Store/Tests/Store.Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Store.Application.Common.Behaviours;
+using Store.Application.Common.Interfaces;
+using Store.Application.StoreSections.Commands.CreateStoreSection;
+
+namespace Store.Application.UnitTests.Common.Behaviours
+{
+    public class RequestLoggerTests
+    {
+        private Mock<ILogger<CreateStoreSectionCommand>> _logger = null!;
+        private Mock<ICurrentUserService> _currentUserService = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new Mock<ILogger<CreateStoreSectionCommand>>();
+            _currentUserService = new Mock<ICurrentUserService>();
+        }
+
+        [Test]
+        public async Task ShouldCallUserIdOnce()
+        {
+            _currentUserService.Setup(x => x.UserId).Returns(Guid.NewGuid().ToString());
+
+            var requestLogger = new LoggingBehaviour<CreateStoreSectionCommand>(_logger.Object, _currentUserService.Object);
+
+            await requestLogger.Process(new CreateStoreSectionCommand { StoreId = Guid.Empty, Name = "Name" }, new CancellationToken());
+
+            _currentUserService.Verify(i => i.UserId, Times.Once);
+        }
+    }
+}
---
+++ Services/Store/Tests/Store.Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Store.Application.Common.Mappings;
+using Store.Application.Stores.Queries;
+using Store.Domain.Entities;
+using System.Runtime.Serialization;
+
+namespace Store.Application.UnitTests.Common.Mappings
+{
+    [TestFixture]
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(config =>
+                config.AddProfile<MappingProfile>());
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Test]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        [TestCase(typeof(Domain.Entities.Store), typeof(StoreDTO))]
+        [TestCase(typeof(StoreSection), typeof(StoreSectionDTO))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = GetInstanceOf(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+
+        private object GetInstanceOf(Type type)
+        {
+            if (type.GetConstructor(Type.EmptyTypes) != null)
+                return Activator.CreateInstance(type)!;
+
+            return FormatterServices.GetUninitializedObject(type);
+        }
+    }
+}
