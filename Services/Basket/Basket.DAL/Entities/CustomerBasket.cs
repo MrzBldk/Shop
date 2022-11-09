d65a4a757f1ade2957448d011de68800601d94be
MrzBldk <anton19lityagin@gmail.com>
2022-11-09 19:20:41 +0300

    Basket microservice data access layer

---
+++ Services/Basket/Basket.DAL/Entities/CustomerBasket.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.DAL.Entities
+{
+    public class CustomerBasket
+    {
+        public string BuyerId { get; set; }
+
+        public List<BasketItem> Items { get; set; } = new();
+
+        public CustomerBasket() { }
+
+        public CustomerBasket(string customerId)
+        {
+            BuyerId = customerId;
+        }
+    }
+}
