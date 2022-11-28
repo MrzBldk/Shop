55e655293bf72d60e67afd12246f057304d6d682
MrzBldk <anton_lityagin@mail.ru>
2022-11-28 09:18:57 +0300

    add swagger authorization

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using Catalog.API;
 using Catalog.API.Mappers;
 using Catalog.BLL.Mappers;
 using Catalog.BLL.Services;
@@ -6,13 +7,54 @@ using Catalog.DAL.Context;
 using Catalog.DAL.Entities;
 using Catalog.DAL.Repositories;
 using Catalog.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    options.Authority = "https://sts.skoruba.local";
+    options.RequireHttpsMetadata = false;
+    options.Audience = "catalog_api";
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CatalogScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "catalog_api");
+        policy.Build();
+    });
+});
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri("https://sts.skoruba.local/connect/authorize"),
+                TokenUrl = new Uri("https://sts.skoruba.local/connect/token"),
+                Scopes = new Dictionary<string, string> { { "catalog_api", "catalog_api" } }
+            }
+        }
+    });
+    options.OperationFilter<AuthorizeCheckOperationFilter>();
+});
 builder.Services.AddAutoMapper(typeof(DtoToViewModelMappingProfile), typeof(ViewModelToDtoMappingProfile),
     typeof(DomainToDtoMappingProfile), typeof(DtoToDomainMappingProfile));
 builder.Services.AddDbContext<CatalogContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
@@ -27,11 +69,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(setup =>
+    {
+        setup.OAuthClientId("catalog_api");
+        setup.OAuthAppName("catalog_api");
+        setup.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
