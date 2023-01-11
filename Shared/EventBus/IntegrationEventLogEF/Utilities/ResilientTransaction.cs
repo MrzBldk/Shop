20d891a678a979752504ee79d6732f53fef2d182
MrzBldk <anton19lityagin@gmail.com>
2023-01-11 19:30:52 +0300

    integration events logging

---
+++ Shared/EventBus/IntegrationEventLogEF/Utilities/ResilientTransaction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace IntegrationEventLogEF.Utilities
+{
+    public class ResilientTransaction
+    {
+        private readonly DbContext _context;
+        private ResilientTransaction(DbContext context) =>
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+        public static ResilientTransaction New(DbContext context) => new(context);
+
+        public async Task ExecuteAsync(Func<Task> action)
+        {
+            IExecutionStrategy strategy = _context.Database.CreateExecutionStrategy();
+            await strategy.ExecuteAsync(async () =>
+            {
+                await using var transaction = await _context.Database.BeginTransactionAsync();
+                await action();
+                await transaction.CommitAsync();
+            });
+        }
+    }
+}
