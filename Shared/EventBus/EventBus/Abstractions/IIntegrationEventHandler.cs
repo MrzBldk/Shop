284f024a4bf6fd162c9f1780896117e738e7bd53
MrzBldk <anton19lityagin@gmail.com>
2023-01-11 17:28:34 +0300

    event bus implementation

---
+++ Shared/EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Events;
+
+namespace EventBus.Abstractions
+{
+
+    public interface IIntegrationEventHandler
+    {
+    }
+
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+}
